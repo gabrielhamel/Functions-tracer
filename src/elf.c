39fa77a8da04c73cca0353ae0c0cd01155ba23e6
Gabriel Hamel <gabriel.hamel@epitech.eu>
2019-04-03 00:24:36 +0200

    Norme

--- src/elf.c
+++ src/elf.c
@@ -11,26 +11,36 @@
 #include <unistd.h>
 #include <string.h>
 
+static char *get_sym(Elf *elf, GElf_Shdr *shdr, Elf_Data *data, long addr)
+{
+    char *ptr;
+    GElf_Sym sym;
+
+    for (size_t i = 0; i < shdr->sh_size / shdr->sh_entsize; i++) {
+        gelf_getsym(data, i, &sym);
+        if ((Elf64_Addr)addr == sym.st_value && sym.st_name) {
+            ptr = elf_strptr(elf, shdr->sh_link, sym.st_name);
+            if (ptr && strlen(ptr))
+                return (ptr);
+        }
+    }
+    return (NULL);
+}
+
 char *find_symname(Elf *elf, long addr)
 {
     Elf_Scn *scn = NULL;
     GElf_Shdr shdr;
     Elf_Data *data = NULL;
-    GElf_Sym sym;
-    char *ptr;
+    char *tmp;
 
     while ((scn = elf_nextscn(elf, scn)) != NULL) {
         gelf_getshdr(scn, &shdr);
-        if (shdr.sh_entsize != 0) {//shdr.sh_type == SHT_SYMTAB) {//(SHT_DYNSYM | SHT_RELA | SHT_STRTAB | SHT_SYMTAB)) {
+        if (shdr.sh_entsize != 0) {
             data = elf_getdata(scn, NULL);
-            for (size_t i = 0; i < shdr.sh_size / shdr.sh_entsize; i++) {
-                gelf_getsym(data, i, &sym);
-                if ((Elf64_Addr)addr == sym.st_value && sym.st_name) {
-                    ptr = elf_strptr(elf, shdr.sh_link, sym.st_name);
-                    if (ptr && strlen(ptr))
-                        return (ptr);
-                }
-            }
+            tmp = get_sym(elf, &shdr, data, addr);
+            if (tmp)
+                return (tmp);
         }
     }
     return (NULL);
